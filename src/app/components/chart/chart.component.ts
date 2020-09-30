d0fc1173091666796cdd49484ea60339e6628e29
Abdul Sattar <abdul.sattar@ad.infosys.com>
2020-09-30 19:13:34 +0530

    Adding the color on the plot area

--- src/app/components/chart/chart.component.ts
+++ src/app/components/chart/chart.component.ts
@@ -51,13 +51,23 @@ export class ChartComponent implements OnInit {
           },
           enableMouseTracking: false,
           name: 'Gamers',
-          data: [0, 2000, 2200, 2600, 3000, {
-            marker: {
-              fillColor: '#f1f075',
-              lineWidth: 3,
-              lineColor: '#f1f075' // inherit from series
-            }, y: 3500
-          }, 4000, 4200, 4600, 4800, 5000, 5500]
+          // data: [41000, 61000, 122020, 162020, 192020, {
+          //   marker: {
+          //     fillColor: '#f1f075',
+          //     lineWidth: 3,
+          //     lineColor: '#f1f075' // inherit from series
+          //   }, y: 102020
+          // }, 222020, 262020, 352020, 382020, 512020, 322020]
+          // data: [70000, 100000, 150000, 161000, 175000,
+          //   {
+          //     marker: {
+          //       fillColor: '#f1f075',
+          //       lineWidth: 3,
+          //       lineColor: '#f1f075' // inherit from series
+          //     }, y: 200000
+          //   }
+          //   , 220000, 217000, 231000, 218000, 235000, 252500]
+            data: [64000, 120000, 125600, 136800, 148000, 162000, 176000, 181600, 192800, 198400, 204000, 218000]
         },
         {
           fillColor: {
@@ -68,7 +78,15 @@ export class ChartComponent implements OnInit {
             ]
           },
           name: 'Revenue',
-          data: [0, 110000, 121000, 143000, 165000, 192500, 220000, 231000, 253000, 264000, 275000, 302500]
+          data: [51200, 96000, 100480, 109440, 166500,
+            {
+              marker: {
+                fillColor: '#FFFF00',
+                lineWidth: 14,
+                lineColor: '#FFFF00' // inherit from series
+              }, y: 200475
+            },
+            242000, 431300, 506100, 523280, 545700, 585875]
         }, {
           fillColor: {
             linearGradient: [0, 0, 0, 300],
@@ -83,40 +101,31 @@ export class ChartComponent implements OnInit {
           fillColor: {
             linearGradient: [0, 0, 0, 300],
             stops: [
-              [0, Highcharts.getOptions().colors[3]],
-              [1, Highcharts.color(Highcharts.getOptions().colors[3]).setOpacity(0.1).get('rgba')]
-            ]
-          },
-          name: 'Variable Cost',
-          data: [0, 70000, 77000, 91000, 105000, 122500, 140000, 147000, 161000, 168000, 175000, 192500]
-        }, {
-          fillColor: {
-            linearGradient: [0, 0, 0, 300],
-            stops: [
               [0, Highcharts.getOptions().colors[4]],
               [1, Highcharts.color(Highcharts.getOptions().colors[4]).setOpacity(0.1).get('rgba')]
             ]
           },
           name: 'Total Cost (Fixed + Variable)',
           data: [80000, 150000, 157000, 171000, 185000, 202500, 220000, 227000, 241000, 248000, 255000, 272500]
-        },
-        {
-          fillColor: {
-            linearGradient: [0, 0, 0, 300],
-            stops: [
-              [0, Highcharts.getOptions().colors[5]],
-              [1, Highcharts.color(Highcharts.getOptions().colors[5]).setOpacity(0.1).get('rgba')]
-            ]
-          },
-          name: 'Profit',
-          data: [0, -20550, -17580, -11640, -5700, {
-            marker: {
-              fillColor: '#f1f075',
-              lineWidth: 3,
-              lineColor: '#f1f075' // inherit from series
-            }, y: 1725
-          }, 59150, 62120, 68060, 71030, 74000, 81425]
         }],
+        // },
+        // {
+        //   fillColor: {
+        //     linearGradient: [0, 0, 0, 300],
+        //     stops: [
+        //       [0, Highcharts.getOptions().colors[5]],
+        //       [1, Highcharts.color(Highcharts.getOptions().colors[5]).setOpacity(0.1).get('rgba')]
+        //     ]
+        //   },
+        //   name: 'Profit',
+        //   data: [0, -20550, -17580, -11640, -5700, {
+        //     marker: {
+        //       fillColor: '#f1f075',
+        //       lineWidth: 3,
+        //       lineColor: '#f1f075' // inherit from series
+        //     }, y: 1725
+        //   }, 59150, 62120, 68060, 71030, 74000, 81425]
+        // }],
       xAxis: {
         categories: ['Jan-21', 'Feb-21', 'Mar-21', 'Apr-21', 'May-21', 'Jun-21', 'Jul-21', 'Aug-21', 'Sep-21', 'Oct-21', 'Nov-21', 'Dec-21'],
         gridLineWidth: 1,
